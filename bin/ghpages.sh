5e4d804e204a0eb252e6b460dd895b39603e6383
xuscrus <jesus.m.cruz.h@gmail.com>
2018-06-09 00:05:34 +0100

    6d42f1a8f3cde28387c652e166e7d27873273d9d

--- bin/ghpages.sh
+++ bin/ghpages.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
-if [ -z "$1" ]
-then
-  echo "Which folder do you want to deploy to GitHub Pages?"
-  exit 1
-fi
-git subtree push --prefix $1 origin gh-pages
+cd dist 
+git add --all
+git commit -m "$(git log '--format=format:%H' master -1)"
+git push origin production --force
--- public/path----82f23e91800fd84fa2c1.js
+++
@@ -1,2 +0,0 @@
-webpackJsonp([60335399758886],{97:function(t,e){t.exports={data:{site:{siteMetadata:{title:"codeGamers"}}},layoutContext:{}}}});
-//# sourceMappingURL=path----82f23e91800fd84fa2c1.js.map
