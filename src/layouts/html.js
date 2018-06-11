814c3226c223064c70903a1ae58ac6901aad1c2a
xuscrus <jesus.m.cruz.h@gmail.com>
2018-06-11 20:39:37 +0100

    chane favicon

--- src/layouts/html.js
+++ src/layouts/html.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import Helmet from 'react-helmet'
 import footer from '../common/footer/index';
+const favicon = require('../data/assets/favicon.png')
 
 export default ({ data, children }) => 
 <div className="site">
 <Helmet>
   <title>{data.title}</title>
+  <link rel="shortcut icon" type="image/png" href={favicon} />
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
   <description>{data.description}</description>
   <link 
     rel="stylesheet" 
