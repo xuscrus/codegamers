5d9775b98906557f4488d3daacca9528f1d01252
xuscrus <jesus.m.cruz.h@gmail.com>
2020-02-15 14:25:16 +0000

    20000d3e1a9ab066bea8a32d2bc57bb3b3277b78

--- src/data/games.js
+++ src/data/games.js
@@ -2,13 +2,13 @@
 export default [
   {
     name: 'Game Of Live',
-    description: 'Apasionante juego de la vida, observa como nacen, crecen, se reproducen y mueren',
+    description: 'Simple rules, complex universe. Based on Coonwey\'s cell game',
     photo: require('./assets/games/gameoflive.png'),
     site: '//gameoflive.codegamers.org'
   },
   {
     name: 'Snake',
-    description: 'Todos recordamos como nuestros dedos se pegaban a los nokia con este juego',
+    description: 'We all remember this nokia game',
     photo: require('./assets/games/snake.png'),
     site: '//snake.codegamers.org'
   }
