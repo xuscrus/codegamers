a4a51ff123fe573f8b83488ea55b19f88947b3cf
xuscrus <jesus.m.cruz.h@gmail.com>
2020-03-25 03:44:21 +0000

    28fd5bdea572e6ff9c7492f0c6f8dbc830bf2671

--- docs/path----11cb62ed25cc6059891f.js
+++ docs/path----11cb62ed25cc6059891f.js
@@ -1,2 +1,2 @@
-webpackJsonp([60335399758886],{95:function(t,e){t.exports={data:{site:{siteMetadata:{title:"CodeGamers"}}},layoutContext:{}}}});
+webpackJsonp([60335399758886],{73:function(t,e){t.exports={data:{site:{siteMetadata:{title:"CodeGamers"}}},layoutContext:{}}}});
 //# sourceMappingURL=path----11cb62ed25cc6059891f.js.map
